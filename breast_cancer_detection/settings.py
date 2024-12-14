33d4310d67eec83c619f01e8dba599f4c446ddf1
Chaitra Adiga <itsca03@gmail.com>
2024-12-14 21:16:17 +0530

    Updated email functionalties

--- breast_cancer_detection/settings.py
+++ breast_cancer_detection/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Use your email provider's SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'breastcancerdetectionsystem@gmail.com'  # Your email address
-EMAIL_HOST_PASSWORD = 'caxwyucnwtjpkhuv'  # Your email password
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
